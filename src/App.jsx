2810c535a3bd7a73a9db718598757cab40280c88
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2022-12-29 11:59:07 +0100

    Add background styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./components/Home"
 
 function App() {
   return (
-    <div className="lg:bg-home-desktop md:bg-home-tablet sm:bg-home-mobile bg-home-mobile min-h-screen">
+    <div className="lg:bg-home-desktop md:bg-home-tablet sm:bg-home-mobile bg-home-mobile min-h-screen bg-no-repeat bg-cover">
       <Home />
     </div>
   )
